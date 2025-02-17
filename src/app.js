095cf975ae64e221be2f583e84fe428d5ded17fd
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-02-17 23:23:41 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+document.querySelector("#excusa").innerHTML = generarExcusa()
 };
+
+
+function generarExcusa(){
+  let who = ['I', 'My neighbor', 'my brother', 'The Police'];
+
+  let action = ['ate', 'peed', 'crushed', 'broke', 'stole'];
+
+  let what = ['my house', 'my phone', 'the car', 'my computer'];
+
+  let when = ['before the class', 'last night', '5 mins ago', 'while I was studying'];
+
+  let sujeto = who[Math.floor(Math.random()*who.length)]
+  let accion = action[Math.floor(Math.random()*action.length)]
+  let elQue = what[Math.floor(Math.random()*what.length)]
+  let cuando = when[Math.floor(Math.random()*when.length)]
+
+  return `${sujeto} ${accion} ${elQue} ${cuando}`
+
+}
+
+
